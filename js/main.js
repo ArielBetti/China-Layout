733b8c1fddc9f34adfd3e716738c412eae72b8da
ArielBetti <ariel.betti@gmail.com>
2019-04-24 20:29:04 -0300

    Arrumando alguns estilos

---
+++ js/main.js
@@ -0,0 +1,19 @@
+let idioma = document.getElementById("idioma");
+
+idioma.children[0].addEventListener("click", (event) => {
+
+	console.log("sabadou");
+
+});
+
+idioma.children[1].addEventListener("click", (event) => {
+
+	console.log("domingou");
+
+});
+
+idioma.children[2].addEventListener("click", (event) => {
+
+	console.log("segundou");
+
+});
